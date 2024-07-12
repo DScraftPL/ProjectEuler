156213416eddbd764399b7013e0d5e9cdffde7c9
DScraftPL <xxxwiacekxxx@gmail.com>
2024-07-12 18:49:30 +0200

    i hate primes i hate primes

--- Problems1-10/Primes.hs
+++ Problems1-10/Primes.hs
@@ -1,1 +1,11 @@
--- module Primes (primeList) where
+module Primes (primeListTo) where
+
+import Data.List
+
+primeListTo :: Integer -> [Integer]
+primeListTo limit = primeListTo' [2 .. limit]
+
+primeListTo' :: [Integer] -> [Integer]
+primeListTo' (x : xs)
+  | null (x : xs) = []
+  | otherwise = x : primeListTo' (xs \\ [x * x, x * (x + 1) ..])
